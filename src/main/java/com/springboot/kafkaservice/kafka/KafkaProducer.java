7440c2e13846a8d95907c38465c7842403ecc941
Sankara Narayanan <69718285+sanknara@users.noreply.github.com>
2022-05-11 19:18:18 +0530

    Update KafkaProducer.java

--- src/main/java/com/springboot/kafkaservice/kafka/KafkaProducer.java
+++ src/main/java/com/springboot/kafkaservice/kafka/KafkaProducer.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaProducer {
     
-    // Property Injection
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
     
